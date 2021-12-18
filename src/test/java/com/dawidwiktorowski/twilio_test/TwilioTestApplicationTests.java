7c8cec8909407122ea6a7ce55aca39b4438abb69
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-18 09:59:11 +0100

    Initial commit

---
+++ src/test/java/com/dawidwiktorowski/twilio_test/TwilioTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawidwiktorowski.twilio_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilioTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
