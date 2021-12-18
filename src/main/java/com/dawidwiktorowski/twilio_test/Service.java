0b4b4ecb29705695bd1976087c1e34a9133299d5
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-18 15:43:44 +0100

    Added service class

---
+++ src/main/java/com/dawidwiktorowski/twilio_test/Service.java
@@ -0,0 +1,18 @@
+package com.dawidwiktorowski.twilio_test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@org.springframework.stereotype.Service
+public class Service {
+
+    private final TwilioSmsSender twilioSmsSender;
+
+    @Autowired
+    public Service(TwilioSmsSender twilioSmsSender) {
+        this.twilioSmsSender = twilioSmsSender;
+    }
+
+    public void sendSms(SmsRequest smsRequest) {
+        twilioSmsSender.sendSms(smsRequest);
+    }
+}
